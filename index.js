617afbb73a4c0f98cce0f65fc4d22884c5f4015c
kelvinachiando <kelvinostin22gmail.com>
2022-06-16 21:10:24 +0300

    commit tree 5359cde805f1e9057e3be3ac62e7572dc0a1d999

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// code your solution here
+// code your solution 
+function saturdayFun(activity) {
+    if (activity) {
+      return `This Saturday, I want to ${activity}!`;
+    } else {
+      return `This Saturday, I want to roller-skate!`;
+    }
+  }
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(string='*') {
+    const innerFunction = function(adjective='special') {
+        return `You are ${string}${adjective}${string}!`
+    }
+    return innerFunction
+}
